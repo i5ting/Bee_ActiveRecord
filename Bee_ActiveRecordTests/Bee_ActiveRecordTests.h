4f159593c7c75360581358a580d14e9eb93190e9
i5ting <i5ting@126.com>
2013-04-16 10:53:03 +0800

    Initial Commit

---
+++ Bee_ActiveRecordTests/Bee_ActiveRecordTests.h
@@ -0,0 +1,13 @@
+//
+//  Bee_ActiveRecordTests.h
+//  Bee_ActiveRecordTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Bee_ActiveRecordTests : SenTestCase
+
+@end
---
+++ Bee_ActiveRecordTests/Bee_ActiveRecordTests.m
@@ -0,0 +1,32 @@
+//
+//  Bee_ActiveRecordTests.m
+//  Bee_ActiveRecordTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import "Bee_ActiveRecordTests.h"
+
+@implementation Bee_ActiveRecordTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Bee_ActiveRecordTests");
+}
+
+@end
